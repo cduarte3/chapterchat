3212a6aa7190e26683d200cda27f1cc4f5c95292
Christian Duarte <cduarte3@myseneca.ca>
2024-07-02 14:19:04 -0400

    removed animation dep, and started login page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Hero() {
+
   return (
     <div className="max-w-[800px] w-full h-screen  mx-auto text-center flex flex-col justify-center">
       <img
         src="logo.png"
         alt="Christian"
-        className="mx-auto rounded-lg lg:w-[50%] animate-jump animate-duration-[1200ms] animate-normal"
+        className="mx-auto rounded-lg md:w-[50%] w-[70%]"
         style={{ display: "block" }}
       ></img>
       <div class="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto sm:p-8">
-        <button className="bg-[rgb(64,63,68)] hover:bg-[rgb(36,36,38)] text-amber-50 font-bold py-4 px-12 rounded text-5xl">
+        <Link to="/login">
+          <button className="bg-[rgb(64,63,68)] hover:bg-[rgb(36,36,38)] text-amber-50 font-bold md:py-4 md:px-12 py-2 px-3 rounded md:text-5xl text-2xl">
           Log In
         </button>
-        <button className="bg-[rgb(64,63,68)] hover:bg-[rgb(36,36,38)] text-amber-50 font-bold py-4 px-12 rounded text-5xl">
+        </Link>
+        
+        <button className="bg-[rgb(64,63,68)] hover:bg-[rgb(36,36,38)] text-amber-50 font-bold md:py-4 md:px-9 py-2 px-3 rounded md:text-5xl text-2xl">
           Sign Up
         </button>
       </div>
---
+++ src/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+import Login from './components/Login';
+import Footer from './components/Footer';
+
+
+function Log() {
+  return (
+    <div className="Login">
+      <Login />
+      <Footer />
+    </div>
+  );
+}
+
+export default Log;
