83da60bb8b7fdea5da24e9d3f8f1a61970ad3539
Christian Duarte <cduarte3@myseneca.ca>
2024-07-02 11:29:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './index.css';
-import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import About from './components/About';
-import Frame from './components/Frame';
 import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <Hero />
-      <About />
-      <Frame />
       <Footer />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Resume from './resume';
 import NotFound from './404';
-import Project from './projects';
-import Feed from './Feed';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,9 +11,6 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/projects" element={<Project />} />
-        <Route path="/feedback" element={<Feed />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
