828c840eca2629e83fda4d7c93b2dbe843d17d4d
Christian Duarte <cduarte3@myseneca.ca>
2025-01-09 13:06:30 -0500

    Fixed mobile menu issue, added footer links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
 
   return (
     <div className="flex justify-center items-center h-24 mx-auto px-4 bg-[rgb(64,63,68)] text-white mt-auto">
-        <h1 className="font-bold">2024 - Christian Duarte</h1>
+      <Link to="https://portv2-three.vercel.app" target="_blank">
+      <h1 className="font-bold pr-4">2024 - Christian Duarte</h1>
+      </Link>
+      <h1 className="font-bold">|</h1>
+      <Link to="/faq">
+        <h1 className="font-bold px-4">FAQ</h1>
+      </Link>
+      <h1 className="font-bold">|</h1>
+      <Link to="/support">
+        <h1 className="font-bold px-4">Support</h1>
+      </Link>
     </div>
   );
 }
